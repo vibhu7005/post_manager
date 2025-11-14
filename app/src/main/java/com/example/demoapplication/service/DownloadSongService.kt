b066368c116e95d10a1a9d540c8346279576d4bf
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2025-11-14 09:30:00 +0530

    Services and handlers

--- app/src/main/java/com/example/demoapplication/service/DownloadSongService.kt
+++ app/src/main/java/com/example/demoapplication/service/DownloadSongService.kt
@@ -3,29 +3,35 @@ package com.example.demoapplication.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.os.Message
 import android.util.Log
-import com.example.demoapplication.AppConstants
 import com.example.demoapplication.AppConstants.TAG
-import java.security.Provider
+import kotlinx.coroutines.*
 //started service
 class DownloadSongService : Service() {
-    lateinit var thread : DownloadThread
 
-    override fun onCreate() {
-        super.onCreate()
-        thread = DownloadThread()
-        thread.start()
-        Thread.sleep(300)
+    private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    
+    companion object {
+        private var isDownloading = false
     }
-
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val songName = intent?.getStringExtra("MUSIC_KEY")
-        val msg = Message.obtain()
-        msg.obj = songName
-        thread.mHandler.sendMessage(msg)
+        songName?.let { 
+            synchronized(this) {
+                serviceScope.launch {
+                    processDownloads(songName)
+                }
+            }
+        }
+
         return START_REDELIVER_INTENT
+    }
+    
+    private suspend fun processDownloads(songName : String) {
+        Log.d(TAG, "downloadSong started: $songName")
+        delay(6000)
+        Log.d(TAG, "downloadedSong: $songName")
     }
 
     override fun onBind(p0: Intent?): IBinder? {
@@ -33,6 +39,7 @@ class DownloadSongService : Service() {
     }
 
     override fun onDestroy() {
+        serviceScope.cancel()
         super.onDestroy()
         Log.d(TAG, "onDestroy: ")
     }
