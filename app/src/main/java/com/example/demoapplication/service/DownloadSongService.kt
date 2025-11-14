908676ffde58f7d175981d0ea15dad2ec546b1a5
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2025-11-14 11:35:20 +0530

    Services and handlers

--- app/src/main/java/com/example/demoapplication/service/DownloadSongService.kt
+++ app/src/main/java/com/example/demoapplication/service/DownloadSongService.kt
@@ -6,29 +6,50 @@ import android.os.IBinder
 import android.util.Log
 import com.example.demoapplication.AppConstants.TAG
 import kotlinx.coroutines.*
+
 //started service
 class DownloadSongService : Service() {
 
     private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     
     companion object {
+        private val downloadQueue = mutableListOf<String>()
         private var isDownloading = false
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val songName = intent?.getStringExtra("MUSIC_KEY")
         songName?.let { 
-            synchronized(this) {
-                serviceScope.launch {
-                    processDownloads(songName)
-                }
+            synchronized(downloadQueue) {
+                downloadQueue.add(it)
+            }
+        }
+        
+        if (!isDownloading) {
+            serviceScope.launch {
+                processDownloads()
             }
         }
 
         return START_REDELIVER_INTENT
     }
     
-    private suspend fun processDownloads(songName : String) {
+    private suspend fun processDownloads() {
+        isDownloading = true
+        
+        while (downloadQueue.isNotEmpty()) {
+            val songName = synchronized(downloadQueue) {
+                if (downloadQueue.isNotEmpty()) downloadQueue.removeAt(0) else null
+            }
+            
+            songName?.let { downloadSong(it) }
+        }
+        
+        isDownloading = false
+        stopSelf()
+    }
+    
+    private suspend fun downloadSong(songName: String) {
         Log.d(TAG, "downloadSong started: $songName")
         delay(6000)
         Log.d(TAG, "downloadedSong: $songName")
