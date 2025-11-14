68fb7c9faeaa1349df1c700fc546afe7e86e6318
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2025-11-14 20:20:30 +0530

    DownloadSongService

--- app/src/main/java/com/example/demoapplication/service/DownloadSongService.kt
+++ app/src/main/java/com/example/demoapplication/service/DownloadSongService.kt
@@ -3,56 +3,27 @@ package com.example.demoapplication.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.os.Message
 import android.util.Log
 import com.example.demoapplication.AppConstants.TAG
-import kotlinx.coroutines.*
-
 //started service
 class DownloadSongService : Service() {
+    lateinit var thread : DownloadThread
 
-    private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    
-    companion object {
-        private val downloadQueue = mutableListOf<String>()
-        private var isDownloading = false
+    override fun onCreate() {
+        super.onCreate()
+        thread = DownloadThread()
+        thread.start()
+        Thread.sleep(300)
     }
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val songName = intent?.getStringExtra("MUSIC_KEY")
-        songName?.let { 
-            synchronized(downloadQueue) {
-                downloadQueue.add(it)
-            }
-        }
-        
-        if (!isDownloading) {
-            serviceScope.launch {
-                processDownloads()
-            }
-        }
-
+        val msg = Message.obtain()
+        msg.obj = songName
+        thread.mHandler.sendMessage(msg)
         return START_REDELIVER_INTENT
-    }
-    
-    private suspend fun processDownloads() {
-        isDownloading = true
-        
-        while (downloadQueue.isNotEmpty()) {
-            val songName = synchronized(downloadQueue) {
-                if (downloadQueue.isNotEmpty()) downloadQueue.removeAt(0) else null
-            }
-            
-            songName?.let { downloadSong(it) }
-        }
-        
-        isDownloading = false
-        stopSelf()
-    }
-    
-    private suspend fun downloadSong(songName: String) {
-        Log.d(TAG, "downloadSong started: $songName")
-        delay(6000)
-        Log.d(TAG, "downloadedSong: $songName")
     }
 
     override fun onBind(p0: Intent?): IBinder? {
@@ -60,7 +31,6 @@ class DownloadSongService : Service() {
     }
 
     override fun onDestroy() {
-        serviceScope.cancel()
         super.onDestroy()
         Log.d(TAG, "onDestroy: ")
     }
