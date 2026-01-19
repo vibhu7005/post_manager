1356bfc2237147a7cf9fc6624ee9ae086820a162
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2026-01-19 22:32:34 +0530

    demo

--- app/src/main/java/com/example/demoapplication/data/Demo.kt
+++ app/src/main/java/com/example/demoapplication/data/Demo.kt
@@ -5,41 +5,42 @@ import kotlin.reflect.KProperty
 // COROUTINE BUILDERS EXPLAINED
 
 fun main() {
-    println("=== Using getValue and setValue (Delegated Properties) ===")
+//    println("=== Using getValue and setValue (Delegated Properties) ===")
+    println("Adi".lambai())
     
-    // Using 'by' keyword - getValue is called when reading
-    var box by Box(10)  // Note: must be 'var' to use setValue
-    println("Initial value: $box")  // Calls getValue() -> prints: 10
-    
-    // setValue is called when assigning
-    box = 20  // Calls setValue() -> updates internal value
-    println("After assignment: $box")  // Calls getValue() -> prints: 20
-    
-    box = 30
-    println("After another assignment: $box")  // Prints: 30
-    
-    println("\n=== Using plus operator ===")
-    
-    // Using the + operator - calls plus() function
-    val box1 = Box(5)
-    val box2 = box1 + 10  // Calls box1.plus(10)
-    println("box1.value = ${box1.value}")  // Still 5 (immutable operation)
-    println("box2.value = ${box2.value}")  // 15 (new Box instance)
-    
-    val box3 = Box(100) + 50 + 25  // Can chain operations
-    println("box3.value = ${box3.value}")  // 175
-    
-    println("\n=== Combining both ===")
-    var mutableBox by Box(1)
-    println("mutableBox = $mutableBox")  // 1
-    
-    // You can't directly use + on delegated property, but you can do:
-    val newBox = Box(mutableBox) + 5
-    println("newBox.value = ${newBox.value}")  // 6
-    
-    // Or update the delegated property
-    mutableBox = mutableBox + 5  // Reads (getValue), adds, then writes (setValue)
-    println("mutableBox after += 5: $mutableBox")  // 6
+//    // Using 'by' keyword - getValue is called when reading
+//    var box by Box(10)  // Note: must be 'var' to use setValue
+//    println("Initial value: $box")  // Calls getValue() -> prints: 10
+//
+//    // setValue is called when assigning
+//    box = 20  // Calls setValue() -> updates internal value
+//    println("After assignment: $box")  // Calls getValue() -> prints: 20
+//
+//    box = 30
+//    println("After another assignment: $box")  // Prints: 30
+//
+//    println("\n=== Using plus operator ===")
+//
+//    // Using the + operator - calls plus() function
+//    val box1 = Box(5)
+//    val box2 = box1 + 10  // Calls box1.plus(10)
+//    println("box1.value = ${box1.value}")  // Still 5 (immutable operation)
+//    println("box2.value = ${box2.value}")  // 15 (new Box instance)
+//
+//    val box3 = Box(100) + 50 + 25  // Can chain operations
+//    println("box3.value = ${box3.value}")  // 175
+//
+//    println("\n=== Combining both ===")
+//    var mutableBox by Box(1)
+//    println("mutableBox = $mutableBox")  // 1
+//
+//    // You can't directly use + on delegated property, but you can do:
+//    val newBox = Box(mutableBox) + 5
+//    println("newBox.value = ${newBox.value}")  // 6
+//
+//    // Or update the delegated property
+//    mutableBox = mutableBox + 5  // Reads (getValue), adds, then writes (setValue)
+//    println("mutableBox after += 5: $mutableBox")  // 6
 }
 
 class DemoClass {
@@ -58,6 +59,11 @@ class Box(var value: Int) {
     operator fun plus(other: Int): Box {
         return Box(this.value + other)
     }
+}
+
+
+fun String.lambai() : Int {
+    return this.length
 }
 
 class SimpleLazy<T>(val initializer: () -> T) {
