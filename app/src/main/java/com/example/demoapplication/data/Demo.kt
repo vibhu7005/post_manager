4795fefb4f37e45ecc3348b7365f0a9fce3e3dbe
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2026-01-20 12:18:06 +0530

    demo

--- app/src/main/java/com/example/demoapplication/data/Demo.kt
+++ app/src/main/java/com/example/demoapplication/data/Demo.kt
@@ -6,8 +6,17 @@ fun main() {
     val result : Result<Person> = Result.Error("dfdfdf")
 }
 
+sealed interface Person {
+    class Student(val name: String) : Person
+    class Teacher(val name: String) : Person
+}
 
-class Person
+sealed interface EmailValidation : Person {
+    class Valid()
+}
+
+
+
 sealed class Result<out T> {
     class Success <T>(val data : T) : Result<T>()
     class Error (val message : String) : Result<Nothing>()
