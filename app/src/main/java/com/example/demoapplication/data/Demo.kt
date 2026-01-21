c919e6b3dad7a769d9f41869fa9c0f5e91322ede
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2026-01-21 09:39:57 +0530

    demo

--- app/src/main/java/com/example/demoapplication/data/Demo.kt
+++ app/src/main/java/com/example/demoapplication/data/Demo.kt
@@ -3,31 +3,15 @@ package com.example.demoapplication.data
 import kotlin.reflect.KProperty
 
 fun main() {
-    val result : Result<Person> = Result.Error("dfdfdf")
 
-    val person = Person.Student("fdf")
-    processPerson(person)
-}
-
-fun processPerson(person: Person) {
-    //exaustive
-    when (person) {
-        is Person.Student -> println("Student Name: ${person.name}")
-        is Person.Teacher -> println("Teacher Name: ${person.name}")
-    }
 }
 
 
+sealed class XY {
 
-sealed interface Person {
-    class Student(val name: String) : Person
-    class Teacher(val name: String) : Person
 }
 
-
-
-
-sealed class Result<out T> {
-    class Success<T>(val data: T) : Result<T>()
-    class Error(val message: String) : Result<Nothing>()
+enum class Network(level: Int) {
+    Nr_5g(1), Lte(4), _3g(3)
 }
+
