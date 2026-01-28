d098d39c500e4fa6baec8893664ecdf8a8c9fed0
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2026-01-28 07:22:27 +0530

    delegate

--- app/src/main/java/com/example/demoapplication/data/general/HigherOrderFunctions.kt
+++ app/src/main/java/com/example/demoapplication/data/general/HigherOrderFunctions.kt
@@ -1,28 +1,54 @@
+import java.lang.Exception
+import kotlin.concurrent.thread
 
 
 fun main() {
     processRecords("Charlie", "Aman", "Bob", "David")
+    val name by Delegate("Vaibhav")
 }
 
 fun processRecords(vararg names: String) {
     for (record in names) {
-        execute(record) {
+        execute(record, block =  {
             if (record == "Bob") {
-                return@processRecords
+                return@execute
             }
             saveRecord(record)
-        }
+        })
     }
 }
 
-inline fun execute(name: String, block: () -> Unit) {
-    val startTime = System.currentTimeMillis()
-    block()
-    val endTime = System.currentTimeMillis()
-    println("Execution time for $name: ${endTime - startTime} ms")
+inline fun execute(name: String, block: () -> Unit, noinline onError: (Exception) -> Unit = {}) {
+    try {
+        val startTime = System.currentTimeMillis()
+        block()
+        val endTime = System.currentTimeMillis()
+        println("Execution time for $name: ${endTime - startTime} ms")
+    } catch (ex: Exception) {
+        val x = onError
+        handleError(x)
+    }
+}
+
+//so to pass function as paramter it need to be marked as noinline
+
+fun handleError(error : (Exception) -> Unit) {
+    val sampleException = Exception("Sample exception")
+    error(sampleException)
 }
 
 fun saveRecord(name: String) {
     println("Saving record for $name")
+}
+
+
+class Delegate(var value: String) {
+    operator fun getValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>): String {
+        return value
+    }
+
+    operator fun setValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>, newValue: String) {
+        value = newValue
+    }
 }
 
