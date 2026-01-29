c7c68397cf50ff7ad997ca66461798b123361a78
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2026-01-29 19:38:49 +0530

    coroutines

--- app/src/main/java/com/example/demoapplication/data/general/Room.kt
+++ app/src/main/java/com/example/demoapplication/data/general/Room.kt
@@ -1,4 +1,24 @@
 package com.example.demoapplication.data.general
 
-class Room {
+import androidx.lifecycle.ViewModel
+import com.example.demoapplication.data.model.Post
+import com.example.demoapplication.data.room.Dao
+import com.example.demoapplication.data.room.Query
+import com.example.demoapplication.data.room.StateFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.stateIn
+
+
+
+
+
+class RoomRepo(RoomDao : RoomDao) {
+    val postDataFlow : Flow<Post> = RoomDao.getPostDataFlow()
+}
+
+@Dao
+interface RoomDao {
+    @Query("SELECT * FROM Post LIMIT 1")
+    fun getPostDataFlow() : Flow<Post>
 }
