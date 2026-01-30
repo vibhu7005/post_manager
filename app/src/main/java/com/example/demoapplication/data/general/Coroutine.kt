5acdf1e95051d1b74320ac8bef192265106e5bee
vaibhavgupta <vaibhav.gupta@moveinsync.com>
2026-01-30 15:29:17 +0530

    coroutines

--- app/src/main/java/com/example/demoapplication/data/general/Coroutine.kt
+++ app/src/main/java/com/example/demoapplication/data/general/Coroutine.kt
@@ -1,33 +1,33 @@
 package com.example.demoapplication.data.general
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val deferredA = async(Dispatchers.IO) { A() }  // Multi-threaded!
-        val deferredB = async (Dispatchers.IO) { B() }   // Multi-threaded!
+        val parentJob = Job()
+        val scope = CoroutineScope(Dispatchers.Default+ parentJob)
+        val job1 = scope.launch {
+            launch { delay(1000)
+            println("child 1")}
+        }
 
-        deferredA.await()
-        deferredB.await()
+        val job2 = scope.launch {
+            delay(2000)
+            println("child2")
+        }
+
+        parentJob.cancel()
+
+        delay(4000)
     }
 
-}
 
-suspend fun A() {
-    for (i in 1..5) {
-        println("A $i")
-        Thread.sleep(100)
-    }
-}
-
-
-suspend fun B() {
-    for (i in 1..5) {
-        println("B $i")
-        Thread.sleep(100)
-    }
 }
